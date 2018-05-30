36be9a96b1c4c3564803674c6208225e90207467
Michał Bar <logout400@gmail.com>
2018-05-30 16:06:31 +0200

    Create GUI for the game

--- src/main/java/me/pumpking/tictactoe/Game.java
+++ src/main/java/me/pumpking/tictactoe/Game.java
@@ -1,7 +1,32 @@
 package me.pumpking.tictactoe;
 
-public class Game {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import me.pumpking.tictactoe.controllers.BoardController;
+import me.pumpking.tictactoe.controllers.GameViewController;
+import me.pumpking.tictactoe.models.Board;
+import me.pumpking.tictactoe.views.GameView;
 
-    public static final int WIDTH = 3;
+public class Game extends Application {
+
+    private BoardController boardController;
+    private GameViewController viewController;
+
+    private static final int SIZE = 3;
+
+    @Override
+    public void init() {
+        boardController = new BoardController(this, new Board(SIZE));
+        viewController = new GameViewController(this, new GameView(SIZE));
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        viewController.showGUI(primaryStage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
---
+++ src/main/java/me/pumpking/tictactoe/controllers/GameViewController.java
@@ -0,0 +1,39 @@
+package me.pumpking.tictactoe.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import me.pumpking.tictactoe.Game;
+import me.pumpking.tictactoe.views.GameView;
+
+public class GameViewController extends Controller implements EventHandler<ActionEvent> {
+
+    private GameView view;
+
+    public GameViewController(Game game, GameView view) {
+        super(game);
+        this.view = view;
+        this.view.setEventListener(this);
+    }
+
+    public void showGUI(Stage stage) {
+        view.showGUI(stage);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (!(event.getSource() instanceof Button)) return;
+        Button button = (Button) event.getSource();
+
+        if (button.getId() != null && button.getId().equalsIgnoreCase("reset")) {
+            // TODO: reset the game
+        } else {
+            int index = (int) button.getProperties().get("index");
+            // TODO: set field
+        }
+
+        event.consume();
+    }
+
+}
